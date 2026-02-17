94f8f5c0477f7c3efb3047698c4702de11c6ae97
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2026-02-17 10:51:31 -0600

    Additional step update

--- src/container/buildAndPush.ps1
+++ src/container/buildAndPush.ps1
@@ -1,14 +1,16 @@
 [CmdletBinding()]
 Param(
-    [Parameter(Position = 1, Mandatory)]
+    [Parameter(Mandatory)]
     [string]$ContainerRegistryLoginServer,
-    [Parameter(Mandatory, Position = 2)]
+    [Parameter(Mandatory)]
     [string]$ImageName
 )
 
 # Download the latest release ZIP of the Azure Naming Tool from GitHub
 
 # Extract the ZIP file
+
+# Modify Dockerfile to enable SSH for App Service
 
 # Run the build tool
 
